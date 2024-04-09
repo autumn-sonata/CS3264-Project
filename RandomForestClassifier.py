3f7b75b3717fa347008b7793c573eae46733724d
chia-yh <0.chiayuhong.0@gmail.com>
2024-04-09 17:41:09 +0800

    Tune hyperparams

--- RandomForestClassifier.py
+++ RandomForestClassifier.py
@@ -11,7 +11,7 @@ y = pd.read_csv("datasets/feature_updated_dataset_y.csv")
 X_train, X_val, y_train, y_val = train_test_split(X, y, test_size=0.2, random_state=69)
 y_train, y_val = y_train.values.ravel(), y_val.values.ravel()
 
-model = RandomForestClassifier()
+model = RandomForestClassifier(n_estimators=100, max_depth=20, min_samples_split=2, min_samples_leaf=1)
 
 model.fit(X_train, y_train)
 y_pred = model.predict(X_val)
@@ -24,4 +24,4 @@ print(f"Accuracy: {accuracy} | Precision: {precision} | Recall: {recall} | f1 sc
 # Per-class metrics
 precision, recall, f1, support = precision_recall_fscore_support(y_val, y_pred)
 for i in range(4):
-    print(f"Class {i}:\nAccuracy: {accuracy[i]} | Precision: {precision[i]} | Recall: {recall[i]} | f1 score: {f1[i]} | {model.__class__.__name__}")
+    print(f"Class {i}:\nPrecision: {precision[i]} | Recall: {recall[i]} | f1 score: {f1[i]} | {model.__class__.__name__}")
